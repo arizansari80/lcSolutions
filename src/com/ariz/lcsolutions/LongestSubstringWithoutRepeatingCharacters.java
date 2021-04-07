82d698ff3839ff6c5264400cb265c66a8b64d498
arizansari80 <arizansari80@gmail.com>
2021-04-07 23:55:08 +0530

    The improvement is from 14.03% to 78.46%, solve issue #1.

--- src/com/ariz/lcsolutions/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/ariz/lcsolutions/LongestSubstringWithoutRepeatingCharacters.java
@@ -12,22 +12,27 @@ public class LongestSubstringWithoutRepeatingCharacters {
 	}
 	
 	private static int lengthOfLongestSubstring(String s) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+		HashMap<Integer,Integer> map = new HashMap<>();
         if (s.isEmpty())
         	return 0;
-        int length = 1;
-        for (int i = 0; i < s.length(); i++) {
+        int maxLength = 1,lastBreakpoint = 0;
+        int i = 0;
+        for (; i < s.length(); i++) {
         	if (map.get((int)s.charAt(i)) != null) {
-        		length = length > map.size() ? length : map.size();
-                i = map.get((int)s.charAt(i));
-                map.clear();
-                continue;
+        		//maxLength = length > maxLength ? length : maxLength;
+        		int val = map.get((int)s.charAt(i));
+        		if (val >= lastBreakpoint && val < i) {
+        			int curLength = i - lastBreakpoint;
+        			maxLength = curLength > maxLength ? curLength : maxLength;
+        			lastBreakpoint = val + 1;
+        		}
         	}
         	map.put((int)s.charAt(i),i);
         }
-        length = length > map.size() ? length : map.size();
+        int curLength = i - lastBreakpoint;
+		maxLength = curLength > maxLength ? curLength : maxLength;
         map.clear();
         map = null;
-        return length;
+        return maxLength;
     }
 }
