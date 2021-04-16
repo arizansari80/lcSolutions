59755ece2645f45115595ed3fa2c0435a7c2eeba
arizansari80 <arizansari80@gmail.com>
2021-04-16 23:44:42 +0530

    Memory Optimize the DP solution but TLE error occurred.

--- src/com/ariz/lcsolutions/Main.java
+++ src/com/ariz/lcsolutions/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		/* System.out.println(ZigZagString.zigZagConvert("A", 2)); */
 		/* System.out.println(StringToInteger.myAtoi("-9223372036854775809")); */
-		int n[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+		int n[] = {  1, 8, 6, 2, 5, 4 , 8, 3, 7 };
 		System.out.println(MostWater.maxArea(n));
 	}
 }
--- src/com/ariz/lcsolutions/MostWater.java
+++ src/com/ariz/lcsolutions/MostWater.java
@@ -1,24 +1,112 @@
 package com.ariz.lcsolutions;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
+class Data {
+	int value;
+	HashSet<Integer> index;
+	
+	Data(final int value,final int index) {
+		this.value = value;
+		this.index = new HashSet<>();
+		this.index.add(index);
+	}
+	
+	void insert(final int index) {
+		this.index.add(index);
+	}
+}
+
+class Node {
+	Data data;
+	Node left;
+	Node right;
+	
+	Node(final int value, final int index) {
+		data = new Data(value,index);
+		left = null;
+		right = null;
+	}
+}
+
+class BinaryTree {
+	Node root;
+	
+	BinaryTree() {
+		root = null;
+	}
+	
+	void insert(final int value, final int index) {
+		if (root == null) {
+			root = new Node(value,index);
+			return;
+		}
+		
+		Node traversal = root;
+		Node temp = null;
+		boolean isLeft = true;
+		while (traversal != null) {
+			if (value == traversal.data.value) {
+				traversal.data.insert(index);
+				return;
+			}
+			temp = traversal;
+			if (value < traversal.data.value) {
+				traversal = traversal.left;
+				isLeft = true;
+			}
+			else {
+				traversal = traversal.right;
+				isLeft = false;
+			}
+		}
+		
+		traversal = new Node(value,index);
+		if (isLeft)
+			temp.left = traversal;
+		else
+			temp.right = traversal;
+	}
+	
+	void getSortedTree(Node root, ArrayList<Data> data) {
+		if (root == null)
+			return;
+		getSortedTree(root.right,data);
+		data.add(root.data);
+		getSortedTree(root.left,data);
+	}
+	
+	
+}
+
 public class MostWater {
 	private static int getArea(final int[] height, final int i, final int j) {
 		return (j - i) * Math.min(height[i],height[j]);
 	}
 	
-	private static int getMaxArea(final int[][] saved, final int i, final int j, final int area) {
-		return Math.max(area, Math.max(saved[i][j - 1], Math.max(saved[i + 1][j], saved[i + 1][j - 1])));
+	private static int getMaxArea(final int[] saved, final int i, final int area) {
+		return Math.max(area, Math.max(saved[i], saved[i + 1]));
 	}
 	
 	public static int maxArea(int[] height) {
 		int n = height.length;
-		int[][] saved = new int[n][n];
-		int maxArea = 0;
+		/*BinaryTree bt = new BinaryTree();
+		for (int i = 0; i < n; i++)
+			bt.insert(height[i], i);*/
 		
+		int maxArea = 0;
+		/*ArrayList<Data> sortedData = new ArrayList<>();
+		bt.getSortedTree(bt.root, sortedData);
+		for (Data elem : sortedData) {
+			
+		}*/
+		int[] saved = new int[n];
 		for (int k = 2; k <= n; k++) {
 			for (int i = 0; i < n - k + 1; i++) {
 				int j = i + k -1;
-				int area = getMaxArea(saved,i,j,getArea(height,i,j));
-				saved[i][j] = area;
+				int area = getMaxArea(saved,i,getArea(height,i,j));
+				saved[i] = area;
 				if (area > maxArea)
 					maxArea = area;
 			}
